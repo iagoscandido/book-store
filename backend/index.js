c537edc1db4937f865c2ff979412125cb6f785b2
iago silva <iago.scandido3@gmail.com>
2025-03-08 22:52:44 -0300

    refact: remove comments

--- backend/index.js
+++ backend/index.js
@@ -3,23 +3,12 @@ import { mongoDBURL, PORT } from "./config.js";
 import mongoose from "mongoose";
 import bookRoutes from "./routes/booksRoutes.js";
 import cors from "cors";
+
 const app = express();
 
-// middleware for parsing request body
+// middlewares
 app.use(express.json());
-
-// middleware for handling CORS Policy
-// option 1: allow all origins with default of cors(*)
 app.use(cors());
-
-// option 2: allow custom origins
-// app.use(
-//   cors({
-//     origin: "http://localohost:3000",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
 
 app.use("/books", bookRoutes);
 
